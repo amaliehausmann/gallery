4595bba366bc5a61348eb348b2298910023557d9
amaliehausmann <amaliehausmann@gmail.com>
2023-11-24 10:19:20 +0100

    6

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ function createGallery() {
     myImages.forEach(function(url) {
         const image = document.createElement('img');
         const article = document.createElement('article');
-        const imageUrl = 'gallery/assets/images/' + url;
+        const imageUrl = 'https://amaliehausmann.github.io/assets/images/' + url;
         image.src = imageUrl;
 
         image.addEventListener('click', function() {
@@ -22,7 +22,7 @@ function createGallery() {
 function singleImageDisplay(url) {
     const singleImageSection = document.getElementById('singleImage');
     const displayedImage = document.getElementById('displayedImage');
-    const displayedUrl = 'gallery/assets/images/' + url;
+    const displayedUrl = 'https://amaliehausmann.github.io/assets/images/' + url;
 
     displayedImage.src = displayedUrl;
     singleImageSection.style.display = 'flex';
