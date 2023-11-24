44770c571ca06a2a71becd89aee3d6646b8c274f
amaliehausmann <amaliehausmann@gmail.com>
2023-11-24 09:31:06 +0100

    galleri med KATTE

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+const myImages = ['cat-1.jpg', 'cat-2.jpg', 'cat-3.jpg','cat-4.jpg', 'cat-5.jpg', 'cat-6.jpg', 'cat-7.jpg', 'cat-8.jpg', 'cat-9.jpg'];
+const myGallery = document.getElementById('gallery');
+
+function createGallery() {
+
+    myImages.forEach(function(url) {
+        const image = document.createElement('img');
+        const article = document.createElement('article');
+        const imageUrl = '/assets/images/' + url;
+        image.src = imageUrl;
+
+        image.addEventListener('click', function() {
+            singleImageDisplay(url);
+            myGallery.style.display = 'none';
+         });
+
+        article.appendChild(image);
+        myGallery.appendChild(article);
+    });
+}
+
+function singleImageDisplay(url) {
+    const singleImageSection = document.getElementById('singleImage');
+    const displayedImage = document.getElementById('displayedImage');
+    const displayedUrl = 'assets/images/' + url;
+
+    displayedImage.src = displayedUrl;
+    singleImageSection.style.display = 'flex';
+
+    displayedImage.addEventListener('click', function() {
+        singleImageSection.style.display = 'none';
+        myGallery.style.display = 'grid'
+    });
+
+}
+
+createGallery();
