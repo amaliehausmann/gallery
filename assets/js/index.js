dcf23bbebfe75eff6dc3b1cfb778a571fcc5388a
amaliehausmann <amaliehausmann@gmail.com>
2023-11-24 09:51:10 +0100

    3

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ function createGallery() {
     myImages.forEach(function(url) {
         const image = document.createElement('img');
         const article = document.createElement('article');
-        const imageUrl = '/assets/images/' + url;
+        const imageUrl = '.../assets/images/' + url;
         image.src = imageUrl;
 
         image.addEventListener('click', function() {
@@ -22,7 +22,7 @@ function createGallery() {
 function singleImageDisplay(url) {
     const singleImageSection = document.getElementById('singleImage');
     const displayedImage = document.getElementById('displayedImage');
-    const displayedUrl = '/assets/images/' + url;
+    const displayedUrl = '.../assets/images/' + url;
 
     displayedImage.src = displayedUrl;
     singleImageSection.style.display = 'flex';
